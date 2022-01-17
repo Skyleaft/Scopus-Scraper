245f50504c6a4727849933bc307c2b00b02b2482
Milzan Malik <milzan_malik@outlook.com>
2022-01-17 15:55:18 +0700

    fix issn & isbn not found

--- connection.py
+++ connection.py
@@ -5,9 +5,9 @@ from tqdm import tqdm
 
 db = mysql.connector.connect(
     host="localhost",
-    port="39000",
-    user="milzandb",
-    passwd="@RootDBCybercode",
+    port="3306",
+    user="root",
+    passwd="",
     database="db_scopus"
 )
 
@@ -15,7 +15,7 @@ db = mysql.connector.connect(
 def loadScopusID():
     cursor = db.cursor()
     if db.is_connected():
-        sql = "SELECT id_scopus FROM author"
+        sql = "SELECT id_scopus FROM author WHERE id_scopus is not null"
         cursor.execute(sql)
         records = cursor.fetchall()
         return records
@@ -31,7 +31,11 @@ def insertPush(data, scopus_id):
                 val = (i['prism:issn'], i['prism:publicationName'],
                        i['prism:aggregationType'], datetime.now(), datetime.now())
             except:
-                val = (i['prism:isbn'][0]['$'], i['prism:publicationName'],
+                try:
+                    val = (i['prism:isbn'][0]['$'], i['prism:publicationName'],
+                       i['prism:aggregationType'], datetime.now(), datetime.now())
+                except:
+                    val = ('', i['prism:publicationName'],
                        i['prism:aggregationType'], datetime.now(), datetime.now())
             cursor.execute(sql, val)
             db.commit()
@@ -46,7 +50,11 @@ def insertPush(data, scopus_id):
                     val = (i['eid'], i['openaccessFlag'], i['dc:title'], i['subtypeDescription'], i['prism:coverDate'], i['prism:volume'], i['citedby-count'],
                            scopus_id, i['prism:issn'], datetime.now(), datetime.now())
                 except:
-                    val = (i['eid'], i['prism:doi'], i['dc:title'], i['subtypeDescription'], i['prism:coverDate'], i['prism:pageRange'], i['citedby-count'],
+                    try:
+                        val = (i['eid'], i['prism:doi'], i['dc:title'], i['subtypeDescription'], i['prism:coverDate'], i['prism:pageRange'], i['citedby-count'],
                            scopus_id, i['prism:isbn'][0]['$'], datetime.now(), datetime.now())
+                    except:
+                        val = (i['eid'], '', i['dc:title'], i['subtypeDescription'], i['prism:coverDate'], i['prism:pageRange'], i['citedby-count'],
+                           scopus_id, '', datetime.now(), datetime.now())
             cursor.execute(sql, val)
             db.commit()
